46a9d071825f5e6d9a9e183198ccc80bab82cc3f
dimka <dimka8445484@gmail.com>
2022-09-09 12:33:45 +0200

    added: add and toggle actions

---
+++ src/store/actions.ts
@@ -0,0 +1,17 @@
+import { TAddTodoAction, TToggleCompleteAction } from "./types";
+
+export const addTodoAction = (
+  text: string,
+  completed: boolean
+): TAddTodoAction => ({
+  type: "ADD_TODO",
+  payload: {
+    text,
+    completed,
+  },
+});
+
+export const toggleCompleteAction = (id: number): TToggleCompleteAction => ({
+  type: "TOGGLE_COMPLETE",
+  payload: id,
+});
---
+++ src/store/types.ts
@@ -0,0 +1,27 @@
+type TTodo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+export type TState = {
+  todo: {
+    isAllTodoMarked: boolean;
+    items: TTodo[];
+  };
+};
+
+export type TAddTodoAction = {
+  type: "ADD_TODO";
+  payload: {
+    text: string;
+    completed: boolean;
+  };
+};
+
+export type TToggleCompleteAction = {
+  type: "TOGGLE_COMPLETE";
+  payload: number;
+};
+
+export type TActions = TAddTodoAction | TToggleCompleteAction;
